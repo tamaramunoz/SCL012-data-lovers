ff469dd72c400f6d8b8e8d8291beba1c7f44a7b9
Camirojasserantoni <camilarojasserantoni@gmail.com>
2019-12-10 20:44:07 -0300

    avances en ReadMe y muestra de tarjetas en pantalla

--- src/data.js
+++ src/data.js
@@ -1,7 +1,30 @@
-/* Manejo de data */
+import POKEMON from './data/pokemon/pokemon.js';//importando función
 
-// esta es una función de ejemplo
+export function filterAZ() {
+  let pokeData = POKEMON.filter(filterAZ => (.name));
+  //console.log(pokeData);
 
-export const example = () => {
-  return 'example';
-};
+ // console.log("POKEDATA;", pokeData)
+ // let maping = pokeData.map(POKEMON[i])//revisar map
+  //console.log(maping)
+  //let sorting = maping.sort()
+  //console.log("SORTING:", sorting);
+  //console.log(sorting)
+  //return sorting;
+}
+
+// todos los pokemons searching by type
+ export let filterByType = (POKEMON, types) => {
+  //if (type === 'grass') {
+  return POKEMON.filter(pokeTypes=> pokeTypes.type.includes(types));
+  }
+console.log(filterByType);
+// return pokeData.filter(element) => {
+// return element.type.includes(type);
+// };
+
+
+//  Buscador  by name 
+// let filterByName = (data, searchText) => {
+// return data.filter(element => element.name===name)
+// window.filterByType = filterByType//
--- src/main.js
+++ src/main.js
@@ -1,15 +1,49 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+import POKEMONS from './data/pokemon/pokemon.js';
+//import { filterAZ } from './data.js';
 
-import { example } from './data.js';
 
-console.log(example);
+function getPOKEMONS(POKEMONParam) {
+  console.log(POKEMONParam)
+return `<div class="wrap">
+        <div class="card-wrap">
+            <div class="card">
+            <div class="front">
+             <h4>${POKEMONParam.name}</h4>
+             <img src="${POKEMONParam.img}" alt="imagen pokemon">
+             <p>${POKEMONParam.num}</p>
+            </div>
+           <div class="back"> 
+           <lo> 
+              <li>Type: ${POKEMONParam.type} </li>  
+              <li>Height: ${POKEMONParam.height}</li>
+              <li>Weight : ${POKEMONParam.weight} </li>
+              <li>Weaknesses: ${POKEMONParam.weaknesses}</li>
+            </lo>
+           </div>
+         </div>
+         </div>
+     </div>`;
+}
+let pokemonsHtml = '';
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+for (let i = 0; i < POKEMONS.length; i++) {
+  pokemonsHtml += getPOKEMONS(POKEMONS[i]);
+}
+const pokeData = document.getElementById('info');
+pokeData.innerHTML = pokemonsHtml;
+
+
+//onst selectAZ = document.getElementById("orderABC");
+//selectAZ.addEventListener("change", () => {
+ // document.getElementById('info').innerHTML = filterAZ();
+//});
+
+//const filterAZfunction = filterAZ();
+//for (let i = 0; i < filterAZfunction.length; i++) {
+ // pokemonsHtml += getPOKEMONS(filterAZfunction[i])
+ 
+//hacer un filtro (array filtrado al for con ifs), map//buscar map y su uso 
+//const filterByType = (data, type) => {p
+//if (type === 'all') {
+//return data;
+//}
