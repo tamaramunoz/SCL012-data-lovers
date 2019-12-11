37b6930291be2035e1008306f161f13e7f28f08d
tamaramunoz <tami.skt@gmail.com>
2019-12-10 22:28:33 -0300

    subiendo cambios en el css, html, DOM, JS en proceso

--- src/data.js
+++ src/data.js
@@ -2,7 +2,123 @@
 /* Manejo de data */
 
 // esta es una función de ejemplo
+//export const example = () => {
+ // return 'example';
+//};
+import POKEMON from './data/pokemon/pokemon.js'
 
-export const example = () => {
-  return 'example';
+
+//function para ordenar alfabeticamente A-Z
+export function filterAZ() {
+  let pokeInfo = POKEMON;
+  let mapeando = pokeInfo.map(a => a > b )
+  let ordenando = mapeando.sort((a,b) => a[name].localeCompare(b[name]))
+  return ordenando;
 };
+
+
+  
+
+
+
+
+
+/*
+//mostrando todos los pokemones en pantalla
+export function showingAll() {
+  let result = " ";
+
+  for (let i = 0; i < POKEMON.length; i++) {
+    result += POKEMON;
+
+    console.log(POKEMON.type);
+    return result;
+
+      
+    //console.log(POKEMON.name)
+     
+      };
+ 
+  };
+
+
+
+
+
+
+
+
+
+//function para ordenar alfabeticamente A-Z
+export function filterAZ() {
+  let pokeInfo = POKEMON;
+  let mapeando = pokeInfo.map(nombre => nombre.name)
+  let ordenando = mapeando.sort()
+  return ordenando;
+};
+
+
+
+/*
+//function para ordenar Alfabeticamente al revez Z-A
+export function filterZA() {
+  let pokeData = POKEMON;
+  let maping = pokeData.map(element => element.name)
+  let sorting = maping.sort()
+  let reverting = sorting.reverse()
+  return reverting;
+};
+
+*/
+
+/*
+export function filterType(POKEMON, type){
+  const filtering = POKEMON.filter(element => element.type){
+    let recorriendo = element.includes(type);
+    return filtering;
+};
+*/
+
+
+/*
+//mostrando todos los pokemones en pantalla
+let pokeData = POKEMON
+let result = "";
+
+pokeData.forEach( (element, i) => {
+  let showAll = (Object.values(POKEMON[i].name));
+  document.getElementById("info").innerHTML += showAll
+});
+*/
+
+/*
+let nombres = POKEMON
+for (const datos of POKEMON) {
+  console.log(Object.values(datos))
+};
+
+for (let i = 0; i < POKEMON.length; i++) {
+  let pokeData = document.getElementById("info");
+  pokeData.innerHTML += 
+  ` <p>${POKEMON[i].num}</p>
+    <p>${POKEMON[i].name}</p> 
+    <p>${POKEMON[i].type}</p>
+    <p>${POKEMON[i].weaknesses}</p>` 
+  } 
+
+*/
+
+/*
+export const getPOKEMON = () => {
+  let pokemonsHtml = ''; 
+  //hacer un filtro (array filtrado al for con ifs), map//buscar map y su uso 
+  //const filterByType = (data, type) => {
+  //if (type === 'all') {
+  //return data;
+  //}
+  for (let i = 0; i < POKEMON.length; i++) {
+    pokemonsHtml += getPOKEMON(POKEMON[i])
+    return POKEMON[i];
+  };
+
+  */
--- src/main.js
+++ src/main.js
@@ -1,35 +1,145 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
- import POKEMON from './data/pokemon/pokemon.js'
+import POKEMON from './data/pokemon/pokemon.js'
 
- console.log(POKEMON);
+ //console.log(POKEMON);
  
- console.log(POKEMON[4]);
+ //console.log(POKEMON[5]);
+
+ //mostrando todos los pokemones un una tarjeta
+ function getPOKEMON(POKEMONParam) {
+  //console.log(POKEMONParam);
+  return `<div class="wrap">
+        <div class="card-wrap">
+            <div class="card">
+            <div class="front">
+             <h4>${POKEMONParam.name}</h4>
+             <img class="imagen" src="${POKEMONParam.img}" alt="imagen pokemon">
+             <p>${POKEMONParam.num}</p>
+            </div>
+           <div class="back"> 
+           <lo> 
+              <li>Type: ${POKEMONParam.type} </li>  
+              <li>Height: ${POKEMONParam.height}</li>
+              <li>Weight : ${POKEMONParam.weight} </li>
+              <li>Weaknesses: ${POKEMONParam.weaknesses}</li>
+            </lo>
+           </div>
+         </div>
+         </div>
+     </div>`;
+}
+let pokemonsHtml = ""; 
+
+for (let i = 0; i < POKEMON.length; i++) {
+  pokemonsHtml += getPOKEMON(POKEMON[i])
+  let pokeData = document.getElementById("info");
+  pokeData.innerHTML = pokemonsHtml;
+}
+
+
+ import {filterAZ} from './data.js';
+ const selectAZ = document.getElementById("orderABC");
+  selectAZ.addEventListener("change", () => {
+    //let showAZ= selectAZ.options[selectAZ.selectedIndex].value;
+    document.getElementById("info").innerHTML 
+    getPOKEMON(filterAZ())
+ });
+
+
+
+
+
+ /*
+ import {filterZA} from './data.js';
+const selectZA = document.getElementById("orderABC");
+ selectZA.addEventListener("change", () => {
+   //let showZA= selectZA.options[selectZA.selectedIndex].text;
+   document.getElementById("info").innerHTML = filterZA();
+});
+
+
+
+
+ import {getPOKEMON} from './data.js';
+  const btnAll = document.getElementById("showAll");
+  btnAll.addEventListener("click", function() {
+    document.getElementById("info").innerHTML += getPOKEMON();
+  });
+
+
+/* 
+import {filterType} from './data.js';
+ const selectType = document.getElementById("filterType");
+  selectType.addEventListener("change", () =>{
+    let condition = selectType.options[selectType.selectedIndex].text;
+    document.getElementById("info").innerHTML = filterType();
+  });
+*/
+
+
+
+ /*
+let card = `<div class="wrap">
+<div class="card-wrap">
+    <div class="card">
+    <div class="front">
+     <h4>${POKEMON.name}</h4>
+     <img src="${POKEMON.img}" alt="imagen pokemon">
+     <p>${POKEMON.num}</p>
+    </div>
+   <div class="back"> 
+   <lo> 
+      <li>Type: ${POKEMON.type} </li>  
+      <li>Height: ${POKEMON.height}</li>
+      <li>Weight : ${POKEMON.weight} </li>
+      <li>Weaknesses: ${POKEMON.weaknesses}</li>
+    </lo>
+   </div>
+ </div>
+ </div>
+</div>`;
+*/
 
 /*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
-for (let i = 0; i < POKEMON.length; i++) {
-  let pokeData = document.getElementById("info");
-  pokeData.innerHTML += 
-  ` <p>${POKEMON[i].num}</p>
-    <p>${POKEMON[i].name}</p> `
-  
+function getPOKEMONS(POKEMONParam) {
+  console.log(POKEMONParam);
+  return `<div class="wrap">
+        <div class="card-wrap">
+            <div class="card">
+            <div class="front">
+             <h4>${POKEMONParam.name}</h4>
+             <img class="imagen" src="${POKEMONParam.img}" alt="imagen pokemon">
+             <p>${POKEMONParam.num}</p>
+            </div>
+           <div class="back"> 
+           <lo> 
+              <li>Type: ${POKEMONParam.type} </li>  
+              <li>Height: ${POKEMONParam.height}</li>
+              <li>Weight : ${POKEMONParam.weight} </li>
+              <li>Weaknesses: ${POKEMONParam.weaknesses}</li>
+            </lo>
+           </div>
+         </div>
+         </div>
+     </div>`;
 }
-/*
-function loadDoc() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
-      } 
-    };
-    xhttp.open("GET", "pokemon.js", true);
-    xhttp.send();
-  }
-  */
+let pokemonsHtml = ''; 
+//hacer un filtro (array filtrado al for con ifs), map//buscar map y su uso 
+//const filterByType = (data, type) => {
+//if (type === 'all') {
+//return data;
+//}
+
+for (let i = 0; i < POKEMONS.length; i++) {
+  pokemonsHtml += getPOKEMONS(POKEMONS[i])
+}
+const pokeData = document.getElementById('info');
+pokeData.innerHTML = pokemonsHtml;
+
+
+
+
+
+
+
+
+*/
