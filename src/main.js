712a9ff5a887700a43f968a1a4b46785c674be0c
Camirojasserantoni <camilarojasserantoni@gmail.com>
2019-12-12 12:37:13 -0300

    modificaciones en carpestas y readme

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import POKEMONS from './data/pokemon/pokemon.js';
-//import { filterAZ } from './data.js';
-
 
 function getPOKEMONS(POKEMONParam) {
   console.log(POKEMONParam)
@@ -24,39 +22,10 @@ return `<div class="wrap">
          </div>
      </div>`;
 }
-let pokemonsHtml = '';
 
-let pokemosArray = POKEMONS.sort(function(primero, segundo){
-  if(primero.name < segundo.name){
-    return -1
-  }
-  if(segundo.name < primero.name){
-    return 1
-  }
-  return 0;
-});
-
-for (let i = 0; i < pokemosArray.length; i++) {
-  pokemonsHtml += getPOKEMONS(pokemosArray[i]);
+let pokemonsHtml = ''; 
+for (let i = 0; i < POKEMONS.length; i++) {
+  pokemonsHtml += getPOKEMONS(POKEMONS[i])
 }
 const pokeData = document.getElementById('info');
 pokeData.innerHTML = pokemonsHtml;
-
-
-
-
-
-//llamado Alfabéticamente 
-// selectAZ.addEventListener('change', () => {
- //document.getElementById('info').innerHTML = filterAZ();
-//});
-
-//const filterAZfunction = filterAZ();
-//for (let i = 0; i < filterAZfunction.length; i++) {
- // pokemonsHtml += getPOKEMONS(filterAZfunction[i])
- 
-//hacer un filtro (array filtrado al for con ifs), map//buscar map y su uso 
-//const filterByType = (data, type) => {p
-//if (type === 'all') {
-//return data;
-//}
